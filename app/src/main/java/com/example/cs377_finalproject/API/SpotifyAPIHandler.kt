e8516a03e571fce458aae445be19adfe36496871
Adolfo Lopez Villanueva <al3755@nau.edu>
2026-04-27 01:04:12 -0700

    Spotify Leak
    Got an email saying that they detected Spotify keys expose, so I removed them for now, but when we start to implement features, I can put them back in.

--- app/src/main/java/com/example/cs377_finalproject/API/SpotifyAPIHandler.kt
+++ app/src/main/java/com/example/cs377_finalproject/API/SpotifyAPIHandler.kt
@@ -7,9 +7,8 @@ class SpotifyAPIHandler {
     /* variables needed to help call the Spotify API
         provided by Spotify for Developers
      */
-    private val clientID = "b7967c719d1c4665a67daf121c1c0743"
-    private val clientSecret = "f360c3dfa71a4a72bb00024043584e98"
-
+    // private val clientID = ""
+    // private val clientSecret = ""
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://api.spotify.com/")
         .addConverterFactory(GsonConverterFactory.create())
